2fc6f31827c7d426909a1a7e9e875fa06657a951
Charles Stover <charlesstover@charlesstover.com>
2018-05-22 00:59:40 -0500

    hotfix

--- constants/with-security.js
+++ constants/with-security.js
@@ -1,16 +1,16 @@
 const contentSecurityPolicy = `\
-connect-src: 'none'; \
-default-src: 'none'; \
-font-src: 'none'; \
-form-action: 'none'; \
-frame-src: 'none'; \
-img-src: 'none'; \
-media-src: 'none'; \
-object-src: 'none'; \
-report-to: 'csp-enforce'; \
-report-uri: 'https://cscdn.report-uri.com/r/d/csp/enforce'; \
-script-src: 'none'; \
-style-src: 'none'\
+connect-src 'none'; \
+default-src 'none'; \
+font-src 'none'; \
+form-action 'none'; \
+frame-src 'none'; \
+img-src 'none'; \
+media-src 'none'; \
+object-src 'none'; \
+report-to 'csp-enforce'; \
+report-uri 'https://cscdn.report-uri.com/r/d/csp/enforce'; \
+script-src 'none'; \
+style-src 'none'\
 `;
 
 const reportTo = [
@@ -35,7 +35,7 @@ module.exports = (f) => {
     response.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
     response.setHeader('X-Content-Type-Options', 'nosniff');
     response.setHeader('X-Frame-Options', 'DENY');
-    resposne.setHeader('X-XSS-Protection', '1; mode=block; report=https://cscdn.report-uri.com/r/d/xss/enforce');
+    response.setHeader('X-XSS-Protection', '1; mode=block; report=https://cscdn.report-uri.com/r/d/xss/enforce');
     f(request, response);
   };
 };
